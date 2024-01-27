3cdc58df1daef55eb34d9fef34f0561d1896b9dc
laddeeth <ladde@ladde.se>
2024-01-27 20:37:37 +0100

    Finished up typescript add course and responsive css

--- js/administrate.js
+++ js/administrate.js
@@ -7,17 +7,75 @@ var Settings;
 const drawAdminPage = async () => {
     if (searchParams.has('add')) {
         const div = createDiv();
+        div.classList.add('adminPage');
+        div.append(createButton('<- Tillbaka till kurser', () => {
+            location.href = './minasidor.html';
+        }), createH2('Lägg till ny kurs'));
+        const form = createForm();
+        form.append(createLabel('kursTitel', 'Titel'), createInput('text', 'kursTitel', ''), createLabel('kursDagar', 'Antal dagar'), createInput('number', 'kursDagar', ''), createLabel('kursPris', 'Pris'), createInput('number', 'kursPris', ''), createButton('Lägg till kurs', (e) => {
+            e.preventDefault();
+            addCourse(form);
+        }));
+        div.append(form);
         return div;
     }
     else {
         const courses = await getCourses();
         const div = createDiv();
         div.append(createH2('Kurser & Deltagare'));
+        div.append(createButton('Lägg till ny kurs', () => {
+            location.href = '?add';
+        }));
         div.classList.add('adminPage');
         courses.forEach(async (course) => {
             div.append(await createCourseCard(course));
         });
         return div;
+    }
+};
+const addCourse = async (form) => {
+    const formData = new FormData(form);
+    for (const [name, value] of formData.entries()) {
+        const input = document.querySelector(`input[name="${name}"]`);
+        input ? input.classList.remove('faulty') : false;
+        if (value.toString().length < 1) {
+            const label = document.querySelector(`label[for="${name}"]`);
+            const input = document.querySelector(`input[name="${name}"]`);
+            input ? input.classList.add('faulty') : false;
+            label
+                ? alert(`Du måste fylla i ${label.innerHTML}`)
+                : alert('Nånting gick fel');
+            return;
+        }
+    }
+    const entries = Object.fromEntries(formData.entries());
+    entries.bokningar = [];
+    entries.kursUpplagg = 'På plats';
+    entries.kursBild = 'landingpage.png';
+    entries.kursStart = new Date().toLocaleDateString('sv-SE');
+    entries.beskrivning = 'Ny kurs';
+    entries.djupareBeskrivning =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...';
+    console.log(entries);
+    try {
+        const response = await fetch(Settings.KURSURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(entries),
+        });
+        if (response.ok) {
+            await response.json();
+            alert(`Du har lagt till kursen ${entries.kursTitel}`);
+            location.href = './minasidor.html';
+        }
+        else {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+    }
+    catch (error) {
+        throw new Error(`Ett fel inträffade i add metoden: ${error}`);
     }
 };
 const getCourses = async () => {
@@ -58,7 +116,7 @@ const createSpan = (text) => {
 };
 const createCourseCard = async (kurs) => {
     const div = createDiv();
-    div.append(createSpan(kurs.id + ': ' + kurs.kursTitel), createSpan(`Antal dagar: ${kurs.kursDagar}`), createSpan(`Pris: ${kurs.kursPris}:-`), createH3('Kunder'), createHr());
+    div.append(createSpan(kurs.id + ': ' + kurs.kursTitel), createSpan(`Antal dagar: ${kurs.kursDagar}`), createSpan(`Pris: ${kurs.kursPris}:-`), createH3('Deltagare'), createHr());
     kurs.bokningar.forEach(async (element) => {
         const span = createSpan((await getUser(element[0])).namn +
             ' ' +
@@ -87,6 +145,23 @@ const createButton = (text, action) => {
     button.append(text);
     button.addEventListener('click', action);
     return button;
+};
+const createForm = () => {
+    return document.createElement('form');
+};
+const createLabel = (target, text) => {
+    const label = document.createElement('label');
+    label.append(text);
+    label.setAttribute('for', target);
+    return label;
+};
+const createInput = (type, name, value) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', type);
+    input.setAttribute('id', name);
+    input.setAttribute('name', name);
+    input.setAttribute('value', value);
+    return input;
 };
 export { drawAdminPage };
 //# sourceMappingURL=administrate.js.map
--- ts/administrate.ts
+++ ts/administrate.ts
@@ -8,7 +8,7 @@ enum Settings {
 }
 
 interface Kurs {
-  id: number;
+  readonly id: number;
   bokningar: [number, string][];
   kursTitel: string;
   kursDagar: number;
@@ -24,16 +24,93 @@ interface User {
 const drawAdminPage = async (): Promise<HTMLDivElement> => {
   if (searchParams.has('add')) {
     const div: HTMLDivElement = createDiv();
+    div.classList.add('adminPage');
+    div.append(
+      createButton('<- Tillbaka till kurser', () => {
+        location.href = './minasidor.html';
+      }),
+      createH2('Lägg till ny kurs')
+    );
+    const form = createForm();
+    form.append(
+      createLabel('kursTitel', 'Titel'),
+      createInput('text', 'kursTitel', ''),
+      createLabel('kursDagar', 'Antal dagar'),
+      createInput('number', 'kursDagar', ''),
+      createLabel('kursPris', 'Pris'),
+      createInput('number', 'kursPris', ''),
+      createButton('Lägg till kurs', (e: Event) => {
+        e.preventDefault();
+        addCourse(form);
+      })
+    );
+    div.append(form);
     return div;
   } else {
     const courses: Kurs[] = await getCourses();
     const div: HTMLDivElement = createDiv();
     div.append(createH2('Kurser & Deltagare'));
+    div.append(
+      createButton('Lägg till ny kurs', () => {
+        location.href = '?add';
+      })
+    );
     div.classList.add('adminPage');
     courses.forEach(async (course: Kurs) => {
       div.append(await createCourseCard(course));
     });
     return div;
+  }
+};
+
+const addCourse = async (form: HTMLFormElement): Promise<void> => {
+  const formData: FormData = new FormData(form);
+  for (const [name, value] of formData.entries()) {
+    const input = document.querySelector(`input[name="${name}"]`);
+    input ? input.classList.remove('faulty') : false;
+    if (value.toString().length < 1) {
+      const label: HTMLLabelElement | null =
+        document.querySelector<HTMLLabelElement>(`label[for="${name}"]`);
+      const input: HTMLInputElement | null =
+        document.querySelector<HTMLInputElement>(`input[name="${name}"]`);
+      input ? input.classList.add('faulty') : false;
+      label
+        ? alert(`Du måste fylla i ${label.innerHTML}`)
+        : alert('Nånting gick fel');
+      return;
+    }
+  }
+  const entries: {
+    [k: string]: any;
+  } = Object.fromEntries(formData.entries());
+  entries.bokningar = [];
+  entries.kursUpplagg = 'På plats';
+  entries.kursBild = 'landingpage.png';
+  entries.kursStart = new Date().toLocaleDateString('sv-SE');
+  entries.beskrivning = 'Ny kurs';
+  entries.djupareBeskrivning =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...';
+
+  console.log(entries);
+
+  try {
+    const response = await fetch(Settings.KURSURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(entries),
+    });
+
+    if (response.ok) {
+      await response.json();
+      alert(`Du har lagt till kursen ${entries.kursTitel}`);
+      location.href = './minasidor.html';
+    } else {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    throw new Error(`Ett fel inträffade i add metoden: ${error}`);
   }
 };
 
@@ -79,10 +156,10 @@ const createCourseCard = async (kurs: Kurs): Promise<HTMLDivElement> => {
     createSpan(kurs.id + ': ' + kurs.kursTitel),
     createSpan(`Antal dagar: ${kurs.kursDagar}`),
     createSpan(`Pris: ${kurs.kursPris}:-`),
-    createH3('Kunder'),
+    createH3('Deltagare'),
     createHr()
   );
-  kurs.bokningar.forEach(async (element) => {
+  kurs.bokningar.forEach(async (element: [number, string]) => {
     const span = createSpan(
       (await getUser(element[0])).namn +
         ' ' +
@@ -95,27 +172,51 @@ const createCourseCard = async (kurs: Kurs): Promise<HTMLDivElement> => {
   return div;
 };
 
-const createHr = () => {
+const createHr = (): HTMLHRElement => {
   return document.createElement('hr');
 };
 
-const createH3 = (text: string) => {
+const createH3 = (text: string): HTMLHeadingElement => {
   const h3 = document.createElement('h3');
   h3.append(text);
   return h3;
 };
 
-const createH2 = (text: string) => {
+const createH2 = (text: string): HTMLHeadingElement => {
   const h2 = document.createElement('h2');
   h2.append(text);
   return h2;
 };
 
-const createButton = (text: string, action: () => void) => {
+const createButton = (text: string, action): HTMLButtonElement => {
   const button = document.createElement('button');
   button.append(text);
   button.addEventListener('click', action);
   return button;
+};
+
+const createForm = (): HTMLFormElement => {
+  return document.createElement('form');
+};
+
+const createLabel = (target: string, text: string): HTMLLabelElement => {
+  const label = document.createElement('label');
+  label.append(text);
+  label.setAttribute('for', target);
+  return label;
+};
+
+const createInput = (
+  type: string,
+  name: string,
+  value: string
+): HTMLInputElement => {
+  const input = document.createElement('input');
+  input.setAttribute('type', type);
+  input.setAttribute('id', name);
+  input.setAttribute('name', name);
+  input.setAttribute('value', value);
+  return input;
 };
 
 export { drawAdminPage };
